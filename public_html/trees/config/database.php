509c234c0070fa83aeef9736f7ed6c4920ce21c2
laith260000-png <laith260000@gmail.com>
2026-02-24 21:34:23 +0300

    Add database configuration file

---
+++ public_html/trees/config/database.php
@@ -0,0 +1,12 @@
+<?php
+// config/database.php
+
+return [
+    'host'      => 'xxx',
+    'dbname'    => 'xxx',
+    'username'  => 'xxx',
+    'password'  => 'xxx@',
+    'charset'   => 'utf8mb4',
+];
+
+
